b30344299f5920af9bf4941c2a84c8a6bcdaf5f5
sinnermann <ealijev@gmail.com>
2023-03-28 20:34:56 +0400

    Working at Product List & correcting an error that arose in F12, solution is code to be written in store.js file

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductList.module.scss";
-
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [grid, setGrid] = useState(true);
+  return (
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
+          <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
+          <p>
+            <b>10</b> Products Found.
+          </p>
+        </div>
+        {/* Search Icon*/}
+        <div>
+          <p>Search</p>
+        </div>
+        {/* Search Icon*/}
+        <div className={styles.sort}>
+          <label>Sort by: </label>
+          <select>
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">A - Z</option>
+            <option value="z a">Z - A</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;

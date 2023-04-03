6b3aea6c9c63cb70b1703e684fee6a23f47b3b60
sinnermann <ealijev@gmail.com>
2023-04-03 19:38:38 +0400

    Setting display of number of products & setting the filter of products

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProductList.module.scss";
 import { BsFillGridFill } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
 import Search from "../../search/Search";
 import ProductItem from "../productItem/ProductItem";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  FILTER_BY_SEARCH,
+  selectFilteredProducts,
+  SORT_PRODUCTS,
+} from "../../../redux/slice/filterSlice";
 
 const ProductList = ({ products }) => {
   const [grid, setGrid] = useState(true);
   const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("latest");
+  const filteredProducts = useSelector(selectFilteredProducts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(SORT_PRODUCTS({ products: products, sort: sort }));
+  }, [dispatch, products, sort]);
+
+  useEffect(() => {
+    dispatch(FILTER_BY_SEARCH({ products: products, search: search }));
+  }, [dispatch, products, search]);
+
   return (
     <div className={styles["product-list"]} id="product">
       <div className={styles.top}>
@@ -19,7 +38,7 @@ const ProductList = ({ products }) => {
           />
           <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
           <p>
-            <b>10</b> Products Found.
+            <b>{filteredProducts.length}</b> Products Found.
           </p>
         </div>
         {/* Search Icon*/}
@@ -29,21 +48,21 @@ const ProductList = ({ products }) => {
         {/* Search Icon*/}
         <div className={styles.sort}>
           <label>Sort by: </label>
-          <select>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
             <option value="latest">Latest</option>
             <option value="lowest-price">Lowest Price</option>
             <option value="highest-price">Highest Price</option>
             <option value="a-z">A - Z</option>
-            <option value="z a">Z - A</option>
+            <option value="z-a">Z - A</option>
           </select>
         </div>
       </div>
       <div className={grid ? `${styles.grid}` : `${styles.list}`}>
-        {products.length === 0 ? (
+        {filteredProducts.length === 0 ? (
           <p>No Product Found.</p>
         ) : (
           <>
-            {products.map((product) => {
+            {filteredProducts.map((product) => {
               return (
                 <div key={product.id}>
                   <ProductItem {...product} grid={grid} product={product} />

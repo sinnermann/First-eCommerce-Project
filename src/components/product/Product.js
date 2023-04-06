e5973630956207349394d41e91eaea03946b9d2e
sinnermann <ealijev@gmail.com>
2023-04-06 20:40:56 +0400

    Making filter responsive for phone

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,24 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import useFetchCollection from "../../customHooks/useFetchCollection";
+import {
+  GET_PRICE_RANGE,
+  selectProducts,
+  STORE_PRODUCTS,
+} from "../../redux/slice/productSlice";
 import styles from "./Product.module.scss";
 import ProductFilter from "./productFilter/ProductFilter";
 import ProductList from "./productList/ProductList";
-import useFetchCollection from "../../customHooks/useFetchCollection";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  STORE_PRODUCTS,
-  GET_PRICE_RANGE,
-} from "../../redux/slice/productSlice";
-import { selectProducts } from "../../redux/slice/productSlice";
 import spinnerImg from "../../assets/spinner.jpg";
+import { FaCogs } from "react-icons/fa";
 
 const Product = () => {
   const { data, isLoading } = useFetchCollection("products");
+  const [showFilter, setShowFilter] = useState(false);
   const products = useSelector(selectProducts);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(STORE_PRODUCTS({ products: data }));
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
 
     dispatch(
       GET_PRICE_RANGE({
@@ -26,23 +31,38 @@ const Product = () => {
       })
     );
   }, [dispatch, data]);
+
+  const toggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
+
   return (
     <section>
       <div className={`container ${styles.product}`}>
-        <aside className={styles.filter}>
+        <aside
+          className={
+            showFilter ? `${styles.filter} ${styles.show}` : `${styles.filter}`
+          }
+        >
           {isLoading ? null : <ProductFilter />}
         </aside>
         <div className={styles.content}>
           {isLoading ? (
             <img
               src={spinnerImg}
-              alt="spinner-image"
+              alt="Loading.."
               style={{ width: "50px" }}
               className="--center-all"
             />
           ) : (
             <ProductList products={products} />
           )}
+          <div className={styles.icon} onClick={toggleFilter}>
+            <FaCogs size={20} color="orangered" />
+            <p>
+              <b>{showFilter ? "Hide Filter" : "Show Filter"}</b>
+            </p>
+          </div>
         </div>
       </div>
     </section>

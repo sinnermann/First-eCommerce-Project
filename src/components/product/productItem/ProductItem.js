5d599f4a3247c24186a2014f2f8ac2329b103694
sinnermann <ealijev@gmail.com>
2023-04-10 18:57:30 +0400

    working at adding items to the cart

--- src/components/product/productItem/ProductItem.js
+++ src/components/product/productItem/ProductItem.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { ADD_TO_CART } from "../../../redux/slice/cartSlice";
 import Card from "../../card/Card";
 import styles from "./ProductItem.module.scss";
 
 const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const dispatch = useDispatch();
   const shortenText = (text, n) => {
     if (text.length > n) {
       const shortenedText = text.substring(0, n).concat("...");
       return shortenedText;
     }
     return text;
+  };
+
+  const addToCart = (product) => {
+    dispatch(ADD_TO_CART(product));
   };
   return (
     <Card cardClass={grid ? `${styles.grid}` : `${styles.list}`}>
@@ -24,7 +31,12 @@ const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
           <h4>{shortenText(name, 18)}</h4>
         </div>
         {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
-        <button className="--btn --btn-danger">Add To Cart</button>
+        <button
+          className="--btn --btn-danger"
+          onClick={() => addToCart(product)}
+        >
+          Add To Cart
+        </button>
       </div>
     </Card>
   );
---
+++ src/redux/slice/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const initialState = {
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    ADD_TO_CART(state, action) {
+      console.log(action.payload);
+      const productIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (productIndex >= 0) {
+        // Item Already exists in the cart
+        //Increase the cart quantity
+        state.cartItems[productIndex].cartQuantity += 1;
+        toast.info(`${action.payload.name} increased by one`, {
+          position: "top-left",
+        });
+      } else {
+        // Item does not exists in the cart
+        //Add item to the cart
+        const tempProduct = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProduct);
+        toast.success(`${action.payload.name} added to cart`, {
+          position: "top-left",
+        });
+      }
+      //save cart to local storage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+  },
+});
+
+export const { ADD_TO_CART } = cartSlice.actions;
+
+export const selectCartItems = (state) => state.cart.cartItems;
+export const selectCartTotalQuantity = (state) => state.cart.cartTotalQuantity;
+export const selectCartTotalAmount = (state) => state.cart.cartTotalAmount;
+
+export default cartSlice.reducer;
